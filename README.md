e63b6befd311c69f2fc44c0315463d7f191e7848
Chaos <97273461+EverlastingChaos@users.noreply.github.com>
2022-08-24 18:08:00 +0530

    unoptimized pseudo text generator

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Text Generator
+You will create a program that can predict the next word in a pseudo-sentence based on the previous words in the sequence and the data that is used to create a statistical model. You will get a deeper understanding of natural language processing and preprocessing with the NLTK library, string operations, and the application of statistics in your code.
+
+https://hyperskill.org/projects/134?track=2
---
+++ main.py
@@ -0,0 +1,55 @@
+import re
+import random
+from nltk import WhitespaceTokenizer, trigrams
+from collections import Counter
+
+
+def tokenize(corpus):
+    with open(corpus) as f:
+        text = f.read()
+        return WhitespaceTokenizer().tokenize(text)
+
+
+def generate_trigrams(tokens):
+    trigrams_ = trigrams(tokens)
+    trigrams_list = [' '.join(x) for x in trigrams_]
+    return trigrams_list
+
+
+def generate_dictionary(list_of_trigrams):
+    new_dict = {}
+    for word in list_of_trigrams:
+        new_dict.setdefault(word[0], []).append(word[1])
+    return new_dict
+
+
+def is_starting_word(word):
+    starting_word_template = "^[A-Z]+[a-z']+.[^.?!]$"
+    return bool(re.match(starting_word_template, word))
+
+
+def is_ending_word(word):
+    ending_word_template = ".*[?!.]"
+    return bool(re.match(ending_word_template, word))
+
+
+filename = input()
+list_of_tokens = tokenize(filename)
+trigrams_in_corpus = generate_trigrams(list_of_tokens)
+lst = [(obj.split()[0] + ' ' + obj.split()[1], obj.split()[2]) for obj in trigrams_in_corpus]
+data = generate_dictionary(lst)
+starting_words = [word[0] for word in lst if is_starting_word(word[0])]
+for i in range(10):
+    sentence = list()
+    first_word = random.choice(starting_words)
+    sentence.append(first_word)
+    while True:
+        tail = data[first_word]
+        freq_tail = Counter(tail)
+        tail_weights = [freq_tail[value] for value in tail]
+        second_word = ''.join(random.choices(tail, weights=tail_weights))
+        if len(sentence) >= 4 and is_ending_word(sentence[-1]):
+            break
+        sentence.append(second_word)
+        first_word = first_word.split()[1] + " " + second_word
+    print(" ".join(sentence))
